4ecab59c2d62feb94f5c38f2b677041dee4c8683
Armands Leinieks <armands.leinieks@gmail.com>
2026-04-27 15:39:23 +0300

    fix null offset deprecation php 8.5

--- src/GettextTranslator.php
+++ src/GettextTranslator.php
@@ -6,10 +6,23 @@ use Gettext\Translator;
 
 class GettextTranslator extends Translator
 {
+    public function __construct()
+    {
+        $this->defaultDomain('');
+    }
+
     protected function getTranslation($domain, $context, $original)
     {
+        $domain = $domain ?? '';
         $context = $context ?? '';
 
         return parent::getTranslation($domain, $context, $original);
+    }
+
+    protected function getPluralIndex($domain, $n, $fallback)
+    {
+        $domain = $domain ?? '';
+
+        return parent::getPluralIndex($domain, $n, $fallback);
     }
 }
